914dd72de7c83c4e54e83d443955117a41b74d41
juan480ps <you@example.comjcorrea480@outlook.com>
2023-07-19 14:23:50 -0400

    a

--- api/__init__.py
+++ api/__init__.py
@@ -2,26 +2,27 @@ import logging.config, yaml
 from flask import Flask
 from flask_restful import Api
 
-logging_file = open('config/log/logging.yaml', 'r')
+logging_file = open('config/log/logging.yaml', 'r')# se lee el archivo de configuracion para el logging
 logging_data = yaml.safe_load(logging_file)
 logging_file.close()
 logging.config.dictConfig(logging_data)
 
-app_config_file = open('config/app_config.yaml', 'r')
+app_config_file = open('config/app_config.yaml', 'r') # se lee el archivo de configuracion para la app. De este archivo se extrae la apikey para poder validar posteriormente, el vencimiento del token y el ambiente de la bd (postgres)
 app_config_data = yaml.safe_load(app_config_file)
 app_config_file.close()
 
 API_KEY = app_config_data['API_KEY']
 AMBIENTE_DB = app_config_data['AMBIENTE_DB']
+VENCIMIENTO_TOKEN = app_config_data['VENCIMIENTO_TOKEN']
 
 app = Flask(__name__)
 api = Api(app)
 
 from api.resources.autenticator import Login, GetApiKeyByAlias
 
-api.add_resource(Login, '/aut/login')
-api.add_resource(GetApiKeyByAlias, '/aut/getapibyalias/<alias>')
+api.add_resource(Login, '/aut/login') # url del servicio principal de autenticacion
+api.add_resource(GetApiKeyByAlias, '/aut/getapibyalias/<alias>') # url para obtener la apikey del postgres mediante el alias de la aplicacion ej. kude => adas68-sdfas554-54sd54
 
-@app.route("/", methods = ['POST', 'GET'])
+@app.route("/", methods = ['POST', 'GET']) # url de prueba
 def hello():
     return "Pagina de prueba"
--- api/resources/autenticator.py
+++ api/resources/autenticator.py
@@ -4,23 +4,19 @@ from flask import request, make_response
 from config.db.db_config_pstgr import postgresqlConfig
 from functools import wraps
 
-connpost = psycopg2.connect(postgresqlConfig)
-vencimiento_token = 30#30
+connpost = psycopg2.connect(postgresqlConfig) # cargamos la configuracion a la bd interna postgres
+vencimiento_token = api.VENCIMIENTO_TOKEN # se asigna el valor del vencimiento del token en el archivo config a una variable
+#variables globales
 access_token = ""
 objetoJson = {}
 arrayJson = []
 
-def generate_token():
+def generate_token(): #funcion para generar token aleatorio
     characters = string.ascii_letters + string.digits
     password = ''.join(random.choice(characters) for i in range(80))
     return password
 
-class APIKeyManager:
-    @staticmethod
-    def validate_api_key(api_key):
-        return api_key == api.API_KEY
-
-def require_api_key(func):
+def require_api_key(func): #funcion para requerir apikey
     @wraps(func)
     def decorated_function(*args, **kwargs):
         try:
@@ -28,7 +24,7 @@ def require_api_key(func):
             data = request.get_json()
             params = data['params']
             api_key = params['apikey']
-            if not api_key or not APIKeyManager.validate_api_key(api_key):
+            if not api_key or not api_key == api.API_KEY:
                 descripcion = 'No autenticado'
                 codigo = -1003
                 objetoJson = []
@@ -52,24 +48,24 @@ def require_api_key(func):
         return func(*args, **kwargs)
     return decorated_function
 
-class Login(Resource):
-    @require_api_key
+class Login(Resource):#clase que se encarga de loguear al cliente
+    @require_api_key #se requiere apikey
     def post(self):
         global vencimiento_token, access_token, objetoJson, arrayJson
-        logging.debug("Entro POST Auth")
+        logging.debug("Entro POST Auth") #logging de prueba
         try:
-            logging.debug("HTTP REQUEST HEADERS: " + str(request.headers))
-            logging.debug("HTTP REQUEST DATA: " + str(request.data))
-            data = request.get_json()
-            logging.info('@REQUEST POST ' + json.dumps(data))            
-            operation = data['operation']
-            params = data['params']            
-            username = params['username']
-            password = params['password']
-            contexto = params['authcontext']            
-            if operation == "get_token":         
-                password = base64.b64encode(password.encode("utf-8")).decode("utf-8")                
-                cursor = connpost.cursor()                
+            logging.debug("HTTP REQUEST HEADERS: " + str(request.headers)) #logging de prueba
+            logging.debug("HTTP REQUEST DATA: " + str(request.data)) #logging de prueba
+            data = request.get_json()# se asigna a una variable el json recibido
+            logging.info('@REQUEST POST ' + json.dumps(data)) #logging de prueba
+            operation = data['operation']# se obtiene la key operacion del json
+            params = data['params']# se obtiene la key params del json
+            username = params['username'] # se obtiene la key username de params
+            password = params['password'] # se obtiene la key password de params
+            contexto = params['authcontext']# se obtiene la key authcontext de params
+            if operation == "get_token": # se valida la operacion a realizar
+                password = base64.b64encode(password.encode("utf-8")).decode("utf-8")# se codifica el password recibido ya que en la bd se guarda de esta manera
+                cursor = connpost.cursor()# abrimos cursor de la bd interna postgres
                 query = f"""SELECT u.nombre username, p.passwd, u.estado est_usuario, r.nombre rol, c.nombre contexto
                             FROM {api.AMBIENTE_DB}.usuario u 
                             join {api.AMBIENTE_DB}."password" p on u.id = p.usuario_id
@@ -81,12 +77,12 @@ class Login(Resource):
                             and u.nombre = '{username}'
                             and p.passwd = '{password}'
                             and c.nombre = '{contexto}';
-                            """
-                logging.debug(str(query))  
-                cursor.execute(query)
-                data = cursor.fetchone()
-                cursor.close()                
-                if data:            
+                            """ # query para vaidar si el usuario esta eutenticado y pertenece al contexto que menciona
+                logging.debug(str(query))#logging de prueba
+                cursor.execute(query) # se ejecuta el query
+                data = cursor.fetchone() # se asigna a una variable la primera fila del resultado del query
+                cursor.close()# se cierra el cursor
+                if data:# se valida si el usuario fue autenticado, en caso afirmativo se devuelve el token autogenerado anteriormente
                     access_token = generate_token()
                     listJson = {
                         'token' : access_token
@@ -97,43 +93,43 @@ class Login(Resource):
                 else:
                     descripcion = 'Usuario no autenticado'
                     codigo = -1003
-                    logging.debug(descripcion)
-                    logging.error("Peticion finalizada con error; " + descripcion + " " + str(codigo), exc_info=True)
+                    logging.debug(descripcion)#logging de prueba
+                    logging.error("Peticion finalizada con error; " + descripcion + " " + str(codigo), exc_info=True)#logging de prueba
             else:
                 descripcion = 'Operación inválida'
                 codigo = -1002
-                logging.debug(descripcion)
-                logging.error("Peticion finalizada con error; " + descripcion + " " + str(codigo), exc_info=True)
+                logging.debug(descripcion)#logging de prueba
+                logging.error("Peticion finalizada con error; " + descripcion + " " + str(codigo), exc_info=True)#logging de prueba
         except KeyError as e :
             descripcion = 'No se encuentra el parametro: ' + str(e)
             codigo = -1001
-            logging.debug(e)
-            logging.error("Peticion finalizada con error; " + descripcion + " " + str(codigo), exc_info=True)
+            logging.debug(e)#logging de prueba
+            logging.error("Peticion finalizada con error; " + descripcion + " " + str(codigo), exc_info=True)#logging de prueba
         except Exception as e:
             descripcion = str(e)
             codigo = -1000
-            logging.debug(e)
-            logging.error("Peticion finalizada con error; " + descripcion + " " + str(codigo), exc_info=True)
+            logging.debug(e)#logging de prueba
+            logging.error("Peticion finalizada con error; " + descripcion + " " + str(codigo), exc_info=True)#logging de prueba
         respuesta = {'codigo': codigo, 'descripcion': descripcion, 'objetoJson': objetoJson, 'arrayJson': arrayJson }        
-        logging.info('@REQUEST GET ' + request.full_path + ' @RESPONSE ' + json.dumps(respuesta))
-        if access_token:
+        logging.info('@REQUEST GET ' + request.full_path + ' @RESPONSE ' + json.dumps(respuesta))#logging de prueba
+        if access_token:# en caso que la autenticacion fue exitosa, se genera cookie de sesion y se devuelve en la respuesta
             respuesta = make_response(respuesta)
             respuesta.set_cookie('cookie', access_token, max_age = vencimiento_token)
         connpost.commit()
         return respuesta
         
-class GetApiKeyByAlias(Resource):
-    @require_api_key
+class GetApiKeyByAlias(Resource):#clase que se encarga de devolver la apikey de acuerdo al alias del ws
+    @require_api_key#se requiere apikey
     def post(self, alias):
         global objetoJson, arrayJson
         try:
-            logging.info('@REQUEST POST GetApiKeyByAlias')
-            cursor = connpost.cursor()
-            query = f"SELECT api_key FROM {api.AMBIENTE_DB}.pool_access WHERE alias = '{alias}' "
-            cursor.execute(query)
-            data = cursor.fetchone()
-            cursor.close()
-            if data:            
+            logging.info('@REQUEST POST GetApiKeyByAlias')#logging de prueba
+            cursor = connpost.cursor()# abrimos cursor de la bd interna postgres
+            query = f"SELECT api_key FROM {api.AMBIENTE_DB}.pool_access WHERE alias = '{alias}' "# query para validar si al app cuenta que apikey asignada
+            cursor.execute(query) # se ejecuta el query
+            data = cursor.fetchone() # se asigna a una variable la primera fila del resultado del query
+            cursor.close()# se cierra el cursor
+            if data:# se valida si la app cuenta esta registrada en la bd, en caso afirmativo se devuelve la apikey
                 api_key_pool = data[0]
                 
                 listJson = {
@@ -145,19 +141,19 @@ class GetApiKeyByAlias(Resource):
             else:
                 descripcion = 'API-Key no encontrado'
                 codigo = -1001
-                logging.debug(descripcion)
-                logging.error("Peticion finalizada con error; " + descripcion + " " + str(codigo), exc_info=True)
+                logging.debug(descripcion)#logging de prueba
+                logging.error("Peticion finalizada con error; " + descripcion + " " + str(codigo), exc_info=True)#logging de prueba
             
         except KeyError as e :
             descripcion = 'No se encuentra el parametro: ' + str(e)
             codigo = -1001
-            logging.debug(e)
-            logging.error("Peticion finalizada con error; " + descripcion + " " + str(codigo), exc_info=True)
+            logging.debug(e)#logging de prueba
+            logging.error("Peticion finalizada con error; " + descripcion + " " + str(codigo), exc_info=True)#logging de prueba
         except Exception as e:
             descripcion = str(e)
             codigo = -1000
-            logging.debug(e)
-            logging.error("Peticion finalizada con error; " + descripcion + " " + str(codigo), exc_info=True)
+            logging.debug(e)#logging de prueba
+            logging.error("Peticion finalizada con error; " + descripcion + " " + str(codigo), exc_info=True)#logging de prueba
         respuesta = {'codigo': codigo, 'descripcion': descripcion, 'objetoJson': objetoJson, 'arrayJson': arrayJson }       
         logging.info('@REQUEST GET ' + request.full_path + ' @RESPONSE ' + json.dumps(respuesta)) 
         connpost.commit()
